62e7dcf9f100688c4a4168b675865b09b32a41c7
kumavis <aaron@kumavis.me>
2021-07-09 12:48:02 -0700

    eslint-plugin - decorate message with identifier name

--- packages/eslint-plugin/lib/rules/no-polymorphic-call.js
+++ packages/eslint-plugin/lib/rules/no-polymorphic-call.js
@@ -21,8 +21,32 @@ module.exports = {
             if (node.callee.type !== 'MemberExpression') {
               return
             }
-            context.report(node, 'Do not use polymorphic calls (unless you truly mean to)')
+            const reportHint = prepareMemberExpressionHint(node.callee)
+            context.report(node, `Polymorphic call: "${reportHint}". May be vulnerable to corruption or trap`)
           }
         }
     },
+}
+
+function prepareMemberExpressionHint (node) {
+  const { object, property, computed } = node
+  let objectHint
+  let propertyHint
+  if (object.type === 'Identifier') {
+    objectHint = object.name
+  } else if (object.type === 'MemberExpression') {
+    objectHint = prepareMemberExpressionHint(object)
+  } else {
+    objectHint = `[[${object.type}]]`
+  }
+  if (property.type === 'Identifier') {
+    if (computed) {
+      propertyHint = `[${property.name}]`
+    } else {
+      propertyHint = property.name
+    }
+  } else {
+    propertyHint = `[[${property.type}]]`
+  }
+  return `${objectHint}.${propertyHint}`
 }
